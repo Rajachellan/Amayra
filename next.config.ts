b76d00683217152e0ac1541fd7e4c43211f830ff
Rajachellan <raja.june@gmail.com>
2026-04-15 12:40:09 +0530

    commit changes

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // ✅ FIX 1: Allow access from other devices
   allowedDevOrigins: ["192.168.6.85"],
-
-
-
   
   // ✅ FIX 2: Allow external images
   images: {
